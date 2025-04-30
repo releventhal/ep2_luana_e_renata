8ef9bc2559db91cb7028fc7d580c38d67499cd3e
luanamr28 <luanamr1@al.insper.edu.br>
2025-04-30 11:19:57 -0300

    ex2 correto

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,11 @@ def rolar_dados(n):
         dado = random.randint(1, 6) 
         lista.append(dado)
     return lista
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    if 0 <= dado_para_guardar < len(dados_rolados):
+        
+        dados_no_estoque.append(dados_rolados[dado_para_guardar])
+        
+        del dados_rolados[dado_para_guardar]
+    return [dados_rolados, dados_no_estoque]
