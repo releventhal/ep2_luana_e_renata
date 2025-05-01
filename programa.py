9bd81a13dd6852784a90ed79e8ec76c8965cacaa
releventhal <renatahl@al.insper.edu.br>
2025-05-01 16:03:58 -0300

    opção 1 funcional

--- programa.py
+++ programa.py
@@ -12,10 +12,19 @@ funcoes.imprime_cartela(cartela)
 
 rodada = 0
 while rodada < 12:
+    dados_rolados = funcoes.rolar_dados(5)
+    dados_guardados = []
+    print(f'Dados rolados: {dados_rolados}')
+    print(f'Dados guardados: {dados_guardados}')
     rodada_continua = True
     while rodada_continua:
-        print(f'Dados rolados: {funcoes.rolar_dados(5)}')
-        print(f'Dados guardados: []')
         print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
         opcao = input(('> '))
-        if opcao == 1: 
+        if opcao == '1': 
+            print("Digite o índice do dado a ser guardado (0 a 4):")
+            dado_guardar = input(('> '))
+            lista_pos_guardar = funcoes.guardar_dado(dados_rolados, dados_guardados, int(dado_guardar))
+            dados_rolados = lista_pos_guardar[0]
+            dados_guardados = lista_pos_guardar[1]
+            print(f'Dados rolados: {dados_rolados}')
+            print(f'Dados guardados: {dados_guardados}')
